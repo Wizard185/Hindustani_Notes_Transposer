12fd84ae8f6ed6c2f5282be69ab5748d04f87cb5
Kishore868 <kishorechandra.cs23@bmsce.ac.in>
2025-07-15 17:46:19 +0530

    Corrected errors

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ const App = () => (
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/reset-password" element={<ResetPassword />} />
             <Route path="/auth/callback" element={<UpdatePasswordForm />} />
+            <Route path="/update-password" element={<UpdatePasswordForm />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
